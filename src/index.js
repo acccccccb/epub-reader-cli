c065b22ea20626ab91c1e8cee3880faa81b19b61
张杨 <tabzhang@foxmail.com>
2024-12-27 14:19:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@
 import os from 'os';
 import fs from 'fs';
 import path from 'path';
-import { DOMParser } from 'xmldom';
 import metaReader from './libs/metaReader.js';
 import chapterReader from './libs/chapterReader.js';
 import 'colors';
 import { Command } from 'commander';
 import { unzipEpub } from './libs/unzipEpub.js';
 import getHash from './libs/getHash.js';
-import { readRecord } from './libs/record.js';
 const currentDir = path.dirname(new URL(import.meta.url).pathname);
 const pkgPath = path.join(currentDir, '../package.json').replace('\\', '');
 const pkgData = fs.readFileSync(pkgPath, 'utf8');
 const pkg = JSON.parse(pkgData);
+const author = pkg.author;
 const programName = pkg.name;
 const version = `v${pkg.version}`;
 const description = pkg.description;
-const author = pkg.author;
 const program = new Command();
-const string = 'Hello, World!';
 
 const tempDir = path.join(os.tmpdir(), '.ebook-reader-cli');
 try {
@@ -28,49 +25,50 @@ try {
     }
 } catch (err) {
     console.error('创建目录失败:', err);
+    process.exit(0);
 }
 
-program.name('ebook-reader-cli');
-program.name('erc');
+program.name(programName);
 program.version(version);
-program.description(description);
+program.description(
+    `${programName} (${version}) ${description}\r\n[${author}]`.blue
+);
+program.usage('erc <epub_file_path>');
+program.helpOption('-h, --help', 'read more information');
 program.showHelpAfterError('错误');
 program.configureOutput({
     // Visibly override write routines as example!
     writeOut: (str) => process.stdout.write(`${str}`),
     writeErr: (str) => process.stdout.write(`[ERR] ${str}`),
     // Highlight errors in color.
-    outputError: (str, write) => process.stdout.write(`[ERR] ${str}`),
+    outputError: (str) => process.stdout.write(`[ERR] ${str}`),
 });
-program
-    .description('输出一条问候消息')
-    .argument('<filePath>', 'epub文件路径')
-    .action(async (filePath) => {
-        const hash = await getHash(filePath);
-        const tempPath = path.join(os.tmpdir(), '.ebook-reader-cli', `${hash}`);
-        const encode = 'utf-8';
-        await unzipEpub(filePath, tempPath);
-        metaReader({
+program.argument('<filePath>', 'epub文件路径').action(async (filePath) => {
+    const hash = await getHash(filePath);
+    const tempPath = path.join(os.tmpdir(), '.ebook-reader-cli', `${hash}`);
+    const encode = 'utf-8';
+    await unzipEpub(filePath, tempPath);
+    metaReader({
+        tempPath,
+        encode,
+    }).then(() => {
+        // console.log(meta);
+        chapterReader({
+            hash,
             tempPath,
             encode,
-        }).then((meta) => {
-            // console.log(meta);
-            chapterReader({
-                hash,
-                tempPath,
-                encode,
-            });
-        });
-
-        process.on('exit', (code) => {
-            try {
-                fs.rmSync(`${tempPath}`, { recursive: true, force: true });
-            } catch (err) {
-                console.error('Error while deleting folder:', err);
-            }
-        });
-        process.on('SIGINT', () => {
-            process.exit(0); // 必须调用 `process.exit()` 结束进程
         });
     });
+
+    process.on('exit', () => {
+        try {
+            fs.rmSync(`${tempPath}`, { recursive: true, force: true });
+        } catch (err) {
+            console.error('Error while deleting folder:', err);
+        }
+    });
+    process.on('SIGINT', () => {
+        process.exit(0); // 必须调用 `process.exit()` 结束进程
+    });
+});
 program.parse();
